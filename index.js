68ac47d2291281ff2413555e3f9cd359f9eefb3a
Hakan Yildiz <hakan@re-coded.com>
2020-04-12 14:44:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ class Deciduous extends Tree{
   static definition(){
     return super.definition() + " Deciduous trees shed their leaves annually.";
   }
-  
-  
+}
+
+class Evergreen extends Tree{
+  constructor(species,name){
+    
+  }
 }
